d9413cc7812a0650807b57ff80f44bc591572d08
kenkao70508 <kenkao70508@gmail.com>
2018-08-24 22:56:14 +0800

    upload

--- upload.py
+++ upload.py
@@ -3,3 +3,5 @@ import os
 os.system('git add .')
 os.system('git commit -m "upload"')
 os.system('git push https://kenkao70508:nw344556@github.com/kenkao70508/BlogTagging-userData.git')
+print("="*30)
+
